9f177c1c2f84c0a3a31d11c907198086aeaf1b20
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-10-01 18:58:38 +1300

    style: update default tailwind styles and colours

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+        fontFamily: {
+          sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        },
+      },
+      colors: {
+        purple: "#574EBA",
+        heading: "#0B2540",
+        body: "#425466",
+        lighter: "#AAB4C1",
+        lightest: "#F6F9FC",
+        darkest: "#0B2540",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
