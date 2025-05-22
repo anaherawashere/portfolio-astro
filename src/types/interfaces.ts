966091c2bf8502387cf6e36dcaa314c49a756a8a
Anahera Foley-Paama <a.foleypaama@gmail.com>
2025-05-22 22:39:23 +1200

    feat: update project interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,26 +1,32 @@
-export interface ProjectProps {
-  date: string;
-  title: string;
-  description: string;
-  company?: string;
-  image: string;
-  href: string[];
-  tech: string[];
-  selected: boolean;
+export interface ProjectLinks {
+  href: string;
+  name: string;
 }
 
-export interface JobProps {
-  startDate: string;
-  endDate: string;
-  role: string;
-  company: string;
+export interface ProjectProps {
+  company?: string;
+  date: string;
   description: string;
-  href: string;
+  href: string[];
+  image: string;
+  links: ProjectLinks[];
+  selected: boolean;
   tech: string[];
+  title: string;
 }
 
 export interface Project {
   frontmatter: ProjectProps;
+}
+
+export interface JobProps {
+  company: string;
+  description: string;
+  endDate: string;
+  href: string;
+  role: string;
+  startDate: string;
+  tech: string[];
 }
 
 export interface Job {
